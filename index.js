ec8fb769ce2c7b7bb5e558d0c7e2abd7c684c67c
github s11142 <shotanagaizumi@gmail.com>
2020-10-11 23:17:43 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var hello = "hello";
+console.log(hello);
+console.log(add(2, 3));
+function add(a, b) {
+    return a + b;
+}
---
+++ index.ts
@@ -0,0 +1,7 @@
+let hello: string = "hello";
+console.log(hello);
+console.log(add(2, 3));
+
+function add(a: number, b: number): number {
+  return a + b;
+}
